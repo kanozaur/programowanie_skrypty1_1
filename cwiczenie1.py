fbd72542193c528bdbdc4f5032c33cb396033e9f
Karolina <kanozaur@gmail.com>
2020-10-27 13:01:25 +0100

    cwiczenie 1

--- cwiczenie1.py
+++ cwiczenie1.py
@@ -1,28 +1,33 @@
-wiek=input("Pierwszy student jest starszy i ma ")
-print("Wiek pierwzego studenta")
-a=input("25")
-print("Wiek drugiego studenta")
-b=input("22")
-if a>b:
-    print("pierwszy student jest starszy i ma "+a+"lata")
+wiek1=input("Wpisz wiek pierwszego sudenta")
+wiek2=input("Wpisz wiek drugiego studenta")
+wiek1=float(wiek1)
+wiek2=float(wiek2)
 
-#wiek drugiego studenta
-plik=open("wiek_drugiego_studenta.txt")
-print(plik)
-tresc=(plik.read)
-print(tresc)
+if wiek1>wiek2:
+    moj_tekst = "Pierwszy student jest starszy i ma " + str(int(wiek1)) + " lat(a)."
+    print(moj_tekst)
 with open("wiek_drugiego_studenta.txt") as plik:
     zawartosc=plik.readlines()
-with open("wiek_drugiego_studenta.txt") as plik:
-    zawartosc=plik.read().splitlines()
-with open("wiek_drugiego_studenta.txt") as plik:
-    zawartosc=[]
-    for wiersz in plik:
-        zawartosc.append(wiersz)
-        zawartosc.append(float(wiersz))
+with_open("wiek_drugiego_studenta.txt","a") as plik:
+    plik.write(moj_tekst)
 
-str(a-b)
-print(a-b)
-int(wiek)
-with_open('moj_plik.txt') as plik:
-plik.write(Pierwszy student jest starszy i ma 25 lat)
+#dosprawdzenia
+#plik=open("wiek_drugiego_studenta.txt")
+#print(plik)
+#tresc=(plik.read)
+#print(tresc)
+#with open("wiek_drugiego_studenta.txt") as plik:
+    #zawartosc=plik.readlines()
+#with open("wiek_drugiego_studenta.txt") as plik:
+    #zawartosc=plik.read().splitlines()
+#with open("wiek_drugiego_studenta.txt") as plik:
+    #zawartosc=[]
+   # for wiersz in plik:
+      #  zawartosc.append(wiersz)
+        #zawartosc.append(float(wiersz))
+
+#str(a-b)
+#print(a-b)
+#int(wiek)
+#
+#a="Pierwszy student jest starszy i ma "+
