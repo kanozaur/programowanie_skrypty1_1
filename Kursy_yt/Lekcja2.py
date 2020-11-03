a4fd3038fff216ad97beac8f8feb4ca395f6e0d2
Karolina <kanozaur@gmail.com>
2020-11-03 23:35:44 +0100

    zadanie2

--- Kursy_yt/Lekcja2.py
+++ Kursy_yt/Lekcja2.py
@@ -1,5 +1,30 @@
 print("Kolejnosc: ")
-print(2+2*2)
+print((2+2)*2)
+print("Dzielenie: ")
+print(5/2)
+print(5//2)
+print("Mnozenie: ")
+print(2*3)
+print(2**3)
+print("Skrocone: ")
+x=5
+x=x+1
+print(x)
+x+=1
+print(x)
+
+print("Konwersja typow: ")
+a=input("Podaj 1 liczbe: ")
+b=input("Podaj 2 liczbe: ")
+print(int(a)+int(b))
+y=2
+z=2
+print(z+y)
+
+
+
+
+
 
 
 
