1d8fe60723b5b217ac8488e02600abbcff884cf5
Karolina <kanozaur@gmail.com>
2020-11-03 12:48:25 +0100

    Wyklad 3, proby wpisywania kodow, plus zolw

---
+++ wyklad_3.py
@@ -0,0 +1,28 @@
+a=5
+def wartosc():
+    a=7
+    print(a)
+print(a)
+wartosc()
+
+liczba=5
+liczba2=liczba
+liczba3=liczba2+7
+print(liczba3)
+napis=liczba
+print(napis)
+nowa_liczba,napis=liczba,"To jest napis tekstowy"
+
+help("modules")
+import turtle
+zolw=turtle.Pen()
+zolw.speed(1)
+zolw.forward(300)
+zolw.left(90)
+zolw.forward(300)
+zolw.left(90)
+zolw.forward(300)
+zolw.left(90)
+zolw.forward(300)
+turtle.done()
+
