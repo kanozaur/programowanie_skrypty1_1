bd3cf3b0c41c4b32e9be3368112802a39aa9237e
Karolina <kanozaur@gmail.com>
2020-11-10 12:45:40 +0100

    cw_3

---
+++ Cwiczenia/Cwiczenia_3/cw_1.py
@@ -0,0 +1,14 @@
+a=21.3
+if isinstance(a,str)==True:
+     print("Typ zmiennej: lancuch.")
+
+if isinstance(a,int)==True:
+    print("Typ zmiennej: calkowita.")
+    print("Zmienna jest wartoscia calkowita.")
+
+if isinstance(a,float)==True:
+    print("Typ zmiennej: rzeczywista.")
+    if a.is_integer()==True:
+        print("Wartosc zmiennej: calkowita.")
+    if a.is_integer()==False:
+        print("Wartosc zmiennej: rzeczywista.")
---
+++ Kursy_yt/Lekcja3.py
@@ -0,0 +1,20 @@
+x=20
+y=20
+
+if x>y:
+    print(" X Wieksze od Y")
+elif x<y:
+    print("X mniejsze od Y")
+else:
+    print("X rowne Y")
+    
+c=20
+d=21
+
+if c>d:
+    print(" X Wieksze od Y")
+elif c<d:
+    print("X mniejsze od Y")
+else:
+    print("X rowne Y")
+
