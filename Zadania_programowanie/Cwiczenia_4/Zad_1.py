74535b793b12ba4f37e4c5a8ffc64b34c142e8c8
Karolina <kanozaur@gmail.com>
2020-11-28 13:08:41 +0100

    problem zad2

--- Zadania_programowanie/Cwiczenia_4/Zad_1.py
+++ Zadania_programowanie/Cwiczenia_4/Zad_1.py
@@ -1,9 +1,13 @@
 #zad1
 import csv
+import numpy as np
 plik='meteo.csv'
 with open('C:/Users/Karolina/Documents/programowanie_skrypty/Zadania_programowanie/Cwiczenia_4/meteo.csv','r') as plik:
     zmienna=plik.readline()
     lista=[zmienna]
+    zmienna=np.array(lista)
+    for row in zmienna:
+        lista.append(row)
     print(lista)
     wiersz2=plik.readlines(2)
     wiersz3=plik.readlines(3)
@@ -15,18 +19,27 @@ with open('C:/Users/Karolina/Documents/programowanie_skrypty/Zadania_programowan
     print(wiersz4)
     print(wiersz5)
     print(wiersz6)
-
-import numpy as np
 lista=[plik]
 tablica=np.array(lista)
 print(tablica)
 #dane meteo dla 4 wiersza?
 print(wiersz4)
 #Dane meteorologiczne dla czwartego wiersza dotyczą Chojnic.
-
 #zad2
-#srednia temp
-srednia_t=[zmienna]
-print(srednia_t[3])
+import statistics
+import math
+t_bialyst=float(tablica[1][5])
+t_bielsko=float(tablica[2][5])
+t_chojn=float(tablica[3][5])
+t_czesto=float(tablica[4][5])
+t_elb=float(tablica[5][5])
+srednia_t=((t_bialyst+t_bielsko+t_chojn+t_czesto+t_elb)/5)
+print(srednia_t)
 #srednia wilg
+#w_bialyst=
+#w_bielsko=
+#w_chojn=
+#w_czesto=
+#w_elb=
+#srednia_w=
 #srednie cisnienie
