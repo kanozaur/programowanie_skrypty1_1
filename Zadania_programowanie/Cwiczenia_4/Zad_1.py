364d7cdbbcf5823022c62236941f8f938c6f9f5b
Karolina <kanozaur@gmail.com>
2020-11-30 20:02:25 +0100

    zakonczone zadanie

--- Zadania_programowanie/Cwiczenia_4/Zad_1.py
+++ Zadania_programowanie/Cwiczenia_4/Zad_1.py
@@ -1,45 +1,31 @@
 #zad1
 import csv
 import numpy as np
-plik='meteo.csv'
-with open('C:/Users/Karolina/Documents/programowanie_skrypty/Zadania_programowanie/Cwiczenia_4/meteo.csv','r') as plik:
-    zmienna=plik.readline()
-    lista=[zmienna]
-    zmienna=np.array(lista)
-    for row in zmienna:
-        lista.append(row)
-    print(lista)
-    wiersz2=plik.readlines(2)
-    wiersz3=plik.readlines(3)
-    wiersz4=plik.readlines(4)
-    wiersz5=plik.readlines(5)
-    wiersz6=plik.readlines(6)
-    print(wiersz2)
-    print(wiersz3)
-    print(wiersz4)
-    print(wiersz5)
-    print(wiersz6)
-lista=[plik]
+plikcsv='C:/Users/Karolina/Documents/programowanie_skrypty/Zadania_programowanie/Cwiczenia_4/meteo.csv'
+lista=[]
+with open(plikcsv,'r') as plik:
+    zmienna=csv.reader(plik)
+    naglowki=next(zmienna)
+    print(naglowki)
+    lista.append(next(zmienna))
+    lista.append(next(zmienna))
+    lista.append(next(zmienna))
+    lista.append(next(zmienna))
+    lista.append(next(zmienna))
+    print(lista[0])
 tablica=np.array(lista)
 print(tablica)
-#dane meteo dla 4 wiersza?
-print(wiersz4)
-#Dane meteorologiczne dla czwartego wiersza dotyczą Chojnic.
+print(tablica[3])
+# Czestochowa
+
 #zad2
-import statistics
-import math
-t_bialyst=float(tablica[1][5])
-t_bielsko=float(tablica[2][5])
-t_chojn=float(tablica[3][5])
-t_czesto=float(tablica[4][5])
-t_elb=float(tablica[5][5])
-srednia_t=((t_bialyst+t_bielsko+t_chojn+t_czesto+t_elb)/5)
-print(srednia_t)
-#srednia wilg
-#w_bialyst=
-#w_bielsko=
-#w_chojn=
-#w_czesto=
-#w_elb=
-#srednia_w=
-#srednie cisnienie
+#wszystkie wiersze, kolumna
+#sr.temp
+print(tablica[:,4].astype(float))
+print(np.mean(tablica[:,4].astype(float)))
+#sr.wilg
+print(tablica[:,7].astype(float))
+print(np.mean(tablica[:,7].astype(float)))
+#sr.cis
+print(tablica[:,-1].astype(float))
+print(np.mean(tablica[:,-1].astype(float)))
